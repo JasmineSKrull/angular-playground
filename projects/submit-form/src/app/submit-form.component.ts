7ab68c9653cb0fb817f202953e1ab522e8bf6bd6
Jasmine Krull <jasminekrull@Jasmines-MacBook-Pro.local>
2021-07-20 14:34:47 -0400

    fix(validation): added empty validation for first and last name inputs

--- projects/submit-form/src/app/submit-form.component.ts
+++ projects/submit-form/src/app/submit-form.component.ts
@@ -10,14 +10,15 @@ export class SubmitFormComponent implements OnInit {
   title = 'Submit Form';
 
   submitForm = new FormGroup({
-    fname: new FormControl(),
-    lname: new FormControl(),
-    pnumber: new FormControl(),
-    email: new FormControl(),
-    descript: new FormControl(),
+    fname: new FormControl('', Validators.required),
+    lname: new FormControl('', Validators.required),
+    pnumber: new FormControl(''),
+    email: new FormControl(''),
+    descript: new FormControl(''),
   });
 
   formValues: any;
+  error: any;
 
   constructor() { }
 
