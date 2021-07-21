4ac3018f1477abac94dd324b814287b751b0230c
Jasmine Krull <jasminekrull@Jasmines-MacBook-Pro.local>
2021-07-21 15:47:26 -0400

    feat(form):added error scenarios, review, and success form

--- projects/submit-form/src/app/submit-form.component.ts
+++ projects/submit-form/src/app/submit-form.component.ts
@@ -13,21 +13,75 @@ export class SubmitFormComponent implements OnInit {
     fname: new FormControl('', Validators.required),
     lname: new FormControl('', Validators.required),
     pnumber: new FormControl(''),
-    email: new FormControl(''),
-    descript: new FormControl(''),
+    email: new FormControl('', Validators.required),
+    descript: new FormControl('', Validators.required),
   });
 
   formValues: any;
-  error: any;
+  error: boolean;
+  formEmpty: boolean;
+  formReview: boolean;
+  formSuccess: boolean;
+  firstName: any;
+  lastName: any;
+  number: any;
+  email: any;
+  description: any;
 
   constructor() { }
 
   submit(){
     this.formValues = this.submitForm.value;
-    console.log(this.formValues)
+    if(
+      this.formValues.fname === '' || 
+      this.formValues.lname === '' || 
+      this.formValues.email === '' || 
+      this.formValues.descript === ''
+      ) {
+      this.error = true
+    } else {
+      this.error = false;
+      this.firstName = this.formValues.fname;
+      this.lastName = this.formValues.lname;
+      this.number = this.formValues.pnumber;
+      this.email = this.formValues.email;
+      this.description = this.formValues.descript;
+      this.formEmpty = false;
+      this.formReview = true;
+      this.formSuccess = false;
+    }
+  }
+
+  clearForm(){
+    this.submitForm = new FormGroup({
+      fname: new FormControl('', Validators.required),
+      lname: new FormControl('', Validators.required),
+      pnumber: new FormControl(''),
+      email: new FormControl('', Validators.required),
+      descript: new FormControl('', Validators.required),
+    });
+  }
+
+  resetForm(){
+    this.clearForm();
+    this.formEmpty = true;
+    this.formReview = false;
+    this.formSuccess = false;
+  }
+
+  success(){
+    this.formEmpty = false;
+    this.formReview = false;
+    this.formSuccess = true;
+  }
+
+  back(){
+    this.formEmpty = true;
+    this.formReview = false;
+    this.formSuccess = false;
   }
 
   ngOnInit(){
-
+    this.formEmpty = true;
   }
 }
